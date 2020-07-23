658918923b8cfdf0e2257eb4207f650276155ec7
kelvin34501 <kelvin34501@gmail.com>
2020-07-23 15:22:15 +0800

    reformat

--- include/EmptyProjectGameState.h
+++ include/EmptyProjectGameState.h
@@ -5,20 +5,18 @@
 #include "OgrePrerequisites.h"
 #include "TutorialGameState.h"
 
-namespace Demo
-{
-	class EmptyProjectGameState : public TutorialGameState
-    {
-        virtual void generateDebugText( float timeSinceLast, Ogre::String &outText );
+namespace Demo {
+    class EmptyProjectGameState : public TutorialGameState {
+        virtual void generateDebugText(float timeSinceLast, Ogre::String &outText);
 
     public:
-		EmptyProjectGameState( const Ogre::String &helpDescription );
+        EmptyProjectGameState(const Ogre::String &helpDescription);
 
         virtual void createScene01(void);
 
-        virtual void update( float timeSinceLast );
+        virtual void update(float timeSinceLast);
 
-        virtual void keyReleased( const SDL_KeyboardEvent &arg );
+        virtual void keyReleased(const SDL_KeyboardEvent &arg);
     };
 }
 
--- src/EmptyProject.cpp
+++ src/EmptyProject.cpp
@@ -1,4 +1,3 @@
-
 #include "GraphicsSystem.h"
 #include "EmptyProjectGameState.h"
 
@@ -14,58 +13,56 @@
 #include "System/MainEntryPoints.h"
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_LINUX
-    #include <unistd.h>
-    #include <sys/types.h>
-    #include <sys/stat.h>
-    #include <pwd.h>
-    #include <errno.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <errno.h>
+
 #endif
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-    #include "shlobj.h"
+#include "shlobj.h"
 #endif
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 INT WINAPI WinMainApp( HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR strCmdLine, INT nCmdShow )
 #else
-int mainApp( int argc, const char *argv[] )
+
+int mainApp(int argc, const char *argv[])
 #endif
 {
-    return Demo::MainEntryPoints::mainAppSingleThreaded( DEMO_MAIN_ENTRY_PARAMS );
+    return Demo::MainEntryPoints::mainAppSingleThreaded(DEMO_MAIN_ENTRY_PARAMS);
 }
 
-namespace Demo
-{
-    class EmptyProjectGraphicsSystem : public GraphicsSystem
-    {
-        virtual Ogre::CompositorWorkspace* setupCompositor()
-        {
+namespace Demo {
+    class EmptyProjectGraphicsSystem : public GraphicsSystem {
+        virtual Ogre::CompositorWorkspace *setupCompositor() {
             return GraphicsSystem::setupCompositor();
         }
 
-        virtual void setupResources(void)
-        {
+        virtual void setupResources(void) {
             GraphicsSystem::setupResources();
 
             Ogre::ConfigFile cf;
             cf.load(mResourcePath + "resources2.cfg");
 
-            Ogre::String dataFolder = cf.getSetting( "DoNotUseAsResource", "Hlms", "" );
+            Ogre::String dataFolder = cf.getSetting("DoNotUseAsResource", "Hlms", "");
 
-            if( dataFolder.empty() )
+            if (dataFolder.empty())
                 dataFolder = "./";
-            else if( *(dataFolder.end() - 1) != '/' )
+            else if (*(dataFolder.end() - 1) != '/')
                 dataFolder += "/";
 
             dataFolder += "2.0/scripts/materials/PbsMaterials";
 
-            addResourceLocation( dataFolder, "FileSystem", "General" );
+            addResourceLocation(dataFolder, "FileSystem", "General");
         }
 
     public:
-        EmptyProjectGraphicsSystem( GameState *gameState ) :
-            GraphicsSystem( gameState )
-        {
+        EmptyProjectGraphicsSystem(GameState *gameState) :
+                GraphicsSystem(gameState) {
             mResourcePath = "../Data/";
 
             //It's recommended that you set this path to:
@@ -86,17 +83,17 @@ namespace Demo
             {
                 //Need to convert to OEM codepage so that fstream can
                 //use it properly on international systems.
-        #if defined(_UNICODE) || defined(UNICODE)
+#if defined(_UNICODE) || defined(UNICODE)
                 int size_needed = WideCharToMultiByte( CP_OEMCP, 0, path, (int)wcslen(path),
                                                        NULL, 0, NULL, NULL );
                 mWriteAccessFolder = std::string( size_needed, 0 );
                 WideCharToMultiByte( CP_OEMCP, 0, path, (int)wcslen(path),
                                      &mWriteAccessFolder[0], size_needed, NULL, NULL );
-        #else
+#else
                 TCHAR oemPath[MAX_PATH];
                 CharToOem( path, oemPath );
                 mWriteAccessFolder = std::string( oemPath );
-        #endif
+#endif
                 mWriteAccessFolder += "/EmptyProject/";
 
                 //Attempt to create directory where config files go
@@ -110,31 +107,27 @@ namespace Demo
             }
 #elif OGRE_PLATFORM == OGRE_PLATFORM_LINUX
             const char *homeDir = getenv("HOME");
-            if( homeDir == 0 )
-                homeDir = getpwuid( getuid() )->pw_dir;
+            if (homeDir == 0)
+                homeDir = getpwuid(getuid())->pw_dir;
             mWriteAccessFolder = homeDir;
             mWriteAccessFolder += "/.config";
-            int result = mkdir( mWriteAccessFolder.c_str(), S_IRWXU|S_IRWXG );
+            int result = mkdir(mWriteAccessFolder.c_str(), S_IRWXU | S_IRWXG);
             int errorReason = errno;
 
             //Create "~/.config"
-            if( result && errorReason != EEXIST )
-            {
-                printf( "Error. Failing to create path '%s'. Do you have access rights?",
-                        mWriteAccessFolder.c_str() );
+            if (result && errorReason != EEXIST) {
+                printf("Error. Failing to create path '%s'. Do you have access rights?",
+                       mWriteAccessFolder.c_str());
                 mWriteAccessFolder = "";
-            }
-            else
-            {
+            } else {
                 //Create "~/.config/EmptyProject"
                 mWriteAccessFolder += "/EmptyProject/";
-                result = mkdir( mWriteAccessFolder.c_str(), S_IRWXU|S_IRWXG );
+                result = mkdir(mWriteAccessFolder.c_str(), S_IRWXU | S_IRWXG);
                 errorReason = errno;
 
-                if( result && errorReason != EEXIST )
-                {
-                    printf( "Error. Failing to create path '%s'. Do you have access rights?",
-                            mWriteAccessFolder.c_str() );
+                if (result && errorReason != EEXIST) {
+                    printf("Error. Failing to create path '%s'. Do you have access rights?",
+                           mWriteAccessFolder.c_str());
                     mWriteAccessFolder = "";
                 }
             }
@@ -155,33 +148,30 @@ namespace Demo
         }
     };
 
-    void MainEntryPoints::createSystems( GameState **outGraphicsGameState,
-                                         GraphicsSystem **outGraphicsSystem,
-                                         GameState **outLogicGameState,
-                                         LogicSystem **outLogicSystem )
-    {
+    void MainEntryPoints::createSystems(GameState **outGraphicsGameState,
+                                        GraphicsSystem **outGraphicsSystem,
+                                        GameState **outLogicGameState,
+                                        LogicSystem **outLogicSystem) {
         EmptyProjectGameState *gfxGameState = new EmptyProjectGameState(
-        "Empty Project Example" );
+                "Empty Project Example");
 
-        GraphicsSystem *graphicsSystem = new EmptyProjectGraphicsSystem( gfxGameState );
+        GraphicsSystem *graphicsSystem = new EmptyProjectGraphicsSystem(gfxGameState);
 
-        gfxGameState->_notifyGraphicsSystem( graphicsSystem );
+        gfxGameState->_notifyGraphicsSystem(graphicsSystem);
 
         *outGraphicsGameState = gfxGameState;
         *outGraphicsSystem = graphicsSystem;
     }
 
-    void MainEntryPoints::destroySystems( GameState *graphicsGameState,
-                                          GraphicsSystem *graphicsSystem,
-                                          GameState *logicGameState,
-                                          LogicSystem *logicSystem )
-    {
+    void MainEntryPoints::destroySystems(GameState *graphicsGameState,
+                                         GraphicsSystem *graphicsSystem,
+                                         GameState *logicGameState,
+                                         LogicSystem *logicSystem) {
         delete graphicsSystem;
         delete graphicsGameState;
     }
 
-    const char* MainEntryPoints::getWindowTitle(void)
-    {
+    const char *MainEntryPoints::getWindowTitle(void) {
         return "Empty Project Sample";
     }
 }
